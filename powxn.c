b06a4046bdb24d2828ef3a1a5ec553c2c8aaca90
qinchao <1187620726@qq.com>
2015-09-08 09:03:07 +0800

    add

---
+++ powxn.c
@@ -0,0 +1,10 @@
+double myPow(double x, int n) {
+        if(n==1)
+		    return x;
+		else if(n==0)
+		    return 1;
+	    if(n%2==0)
+		    return myPow(x*x,n/2);
+	    else 
+		    return myPow(x*x,n/2)*(n>0? x:1/x);
+}
